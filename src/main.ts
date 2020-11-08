8a1173d0e307ec8c97621fb8b72f1522ffd746e2
chili <1032569304@qq.com>
2020-11-08 11:24:36 +0800

    '解决二维码路径问题'

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,15 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+if(document.documentElement.clientWidth>800){
+  window.alert('请使用手机打开本页面，以保证浏览效果');
+  const img = document.createElement('img');
+  img.src = './qrcode.png'
+  img.style.position = 'fixed'
+  img.style.left='50%'
+  img.style.top='50%'
+  img.style.transform='translate(-50%,-50%)'
+img.style.boxShadow='0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img)
+}
